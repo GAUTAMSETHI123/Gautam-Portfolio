c6465d4e046e324d5490648f920c23550daad809
GAUTAMSETHI123 <gautamsethigagan@gmail.com>
2023-02-14 16:25:50 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,8 +1,8 @@
-
+import img from '../assets/gautam.jpg'
 export const headerData = {
-    name: 'Jatin',
-    title: "Full Stack Developer",
+    name: 'Gautam Sethi',
+    title: "Full Stack DevOps Engineer",
     desciption:"Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
-    image: 'null',
+    image: img,
     resumePdf: 'https://jatin-portfolio-bucket.s3.ap-south-1.amazonaws.com/Jatin_Resume.pdf'
 }
