461f244572852fc4a607f5cea6cdaffe9c7bccc8
Gautam <gautamsethigagan@gmail.com>
2023-02-14 13:56:29 +0530

    deploy

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,6 +3,6 @@ export const headerData = {
     name: 'Jatin',
     title: "Full Stack Developer",
     desciption:"Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
-    image: 'https://jatin-portfolio-bucket.s3.ap-south-1.amazonaws.com/PhotoGrid_Plus_1626677425468.jpg',
+    image: 'null',
     resumePdf: 'https://jatin-portfolio-bucket.s3.ap-south-1.amazonaws.com/Jatin_Resume.pdf'
 }
